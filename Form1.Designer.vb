35dba9bd7d51af91a95b0628e274b735b88454ef
dhstoloff <35674470+dhstoloff@users.noreply.github.com>
2018-01-22 17:06:20 -0500

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class PirateFlag
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Flag = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.DeadCaptBox = New System.Windows.Forms.PictureBox()
+        Me.ImageScale = New System.Windows.Forms.PictureBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        CType(Me.Flag, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DeadCaptBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ImageScale, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Flag
+        '
+        Me.Flag.Location = New System.Drawing.Point(79, 41)
+        Me.Flag.Name = "Flag"
+        Me.Flag.Size = New System.Drawing.Size(750, 500)
+        Me.Flag.TabIndex = 0
+        Me.Flag.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(48, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(25, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "500"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(60, 528)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(13, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(76, 544)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(13, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "1"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(804, 554)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(25, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "750"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(344, 9)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(173, 25)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Gold vs Pirates"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(413, 554)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(43, 20)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Gold"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(15, 225)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(58, 20)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "Pirates"
+        '
+        'DeadCaptBox
+        '
+        Me.DeadCaptBox.BackColor = System.Drawing.Color.Black
+        Me.DeadCaptBox.Location = New System.Drawing.Point(835, 78)
+        Me.DeadCaptBox.Name = "DeadCaptBox"
+        Me.DeadCaptBox.Size = New System.Drawing.Size(20, 20)
+        Me.DeadCaptBox.TabIndex = 10
+        Me.DeadCaptBox.TabStop = False
+        '
+        'ImageScale
+        '
+        Me.ImageScale.BackColor = System.Drawing.Color.Black
+        Me.ImageScale.Location = New System.Drawing.Point(835, 115)
+        Me.ImageScale.Name = "ImageScale"
+        Me.ImageScale.Size = New System.Drawing.Size(20, 250)
+        Me.ImageScale.TabIndex = 11
+        Me.ImageScale.TabStop = False
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(861, 78)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(81, 20)
+        Me.Label8.TabIndex = 12
+        Me.Label8.Text = "Dead Cpt."
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(861, 160)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(77, 60)
+        Me.Label9.TabIndex = 13
+        Me.Label9.Text = "Percent" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "of Booty" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Cpt. Gets"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(862, 352)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(21, 13)
+        Me.Label10.TabIndex = 14
+        Me.Label10.Text = "0%"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(861, 115)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(33, 13)
+        Me.Label11.TabIndex = 15
+        Me.Label11.Text = "100%"
+        '
+        'PirateFlag
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1001, 680)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.ImageScale)
+        Me.Controls.Add(Me.DeadCaptBox)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Flag)
+        Me.Name = "PirateFlag"
+        Me.Text = "Form1"
+        CType(Me.Flag, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DeadCaptBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ImageScale, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Flag As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents DeadCaptBox As PictureBox
+    Friend WithEvents ImageScale As PictureBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class PirateFlag
+    Public FlagBM As New Bitmap(750, 500) 'Bitmap that will become the pirate flag
+    Public ScaleBM As New Bitmap(20, 255) 'Bitmap for the scale
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Flagcolor As New Color
+        Dim GoldGivenAway As Integer
+        'Double for loop. There cannot be zero pirates. The dimensions are arbitrary, but should match the flag dimensions.
+        For Gold As Integer = 0 To 749
+            For Pirates As Integer = 1 To 500
+                If Gold > 0 And Gold >= (Pirates / 2) - 1 Then
+                    'The captain needs to give one gold piece to half of the other pirates to survive. The rest they can keep!
+                    GoldGivenAway = (Pirates / 2) - 1
+                    'Yellow is RGB(255,255,0)
+                    'Red is RGB(255,0,0)
+                    Flagcolor = Color.FromArgb(255, 255 - Math.Round((GoldGivenAway / Gold) * 255), 0)
+                ElseIf Math.Round(Math.Log(Pirates - 2 * Gold, 2)) = Math.Log(Pirates - 2 * Gold, 2) Then
+                    'When theirs not enough gold, every 2^x the captain will be safe (because the the next in lines want to save themselves the same fate)
+                    Flagcolor = Color.Red
+                Else
+                    'Captain dies!
+                    Flagcolor = Color.Black
+                End If
+                'VBNet measures xy axis as distance from the upper left, so we need to flip the y-axis
+                FlagBM.SetPixel(Gold, 500 - Pirates, Flagcolor)
+            Next
+        Next
+        Flag.Image = FlagBM
+        For x As Integer = 0 To 19
+            For y As Integer = 0 To 254
+                ScaleBM.SetPixel(x, y, Color.FromArgb(255, 255 - y - 1, 0))
+            Next
+        Next
+        ImageScale.Image = ScaleBM
+    End Sub
+End Class
